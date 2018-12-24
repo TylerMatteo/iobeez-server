89fb8cc652a630bcf89a51851d599bdc2febdfc2
Tyler Matteo <tylermatteo434@gmail.com>
2018-12-24 17:07:44 -0500

    Trying to fix cors more

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 app.use(cors());
 
 const http = require('http').Server(app);
-var io = require('socket.io')(http, {origins:'domain.com:* http://domain.com:* http://www.domain.com:*'});
+var io = require('socket.io')(http, {origins:'*:*'});
 
 // Ideally I would expose endpoints for each temperature individually but for the
 // purposes of this dashboard we can lump them into the same socket
